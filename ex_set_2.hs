7aaf74b90357acdef4b4b4582b717ca9ba354beb
oskar <oskar.vistorskis@gmail.com>
2025-10-22 10:04:30 +0300

    divides second version

--- ex_set_2.hs
+++ ex_set_2.hs
@@ -7,13 +7,20 @@ average array = arraySum array / (fromIntegral (length array))
         arraySum (x:xs) = x + arraySum xs
 
 -- Exercise 2
+isDivisor :: Integer -> Integer -> Bool
+isDivisor num1 num2 = mod num1 num2 == 0
+
 divides :: Integer -> [Integer]
 divides num
     | num <= 0 = []
     | otherwise = getDivisors num []
     where
-        isDivisor n currentNum = mod n currentNum == 0
         getDivisors currentNum arr
             | currentNum <= 0 = arr
             | isDivisor num currentNum = getDivisors (currentNum - 1) (currentNum : arr)
             | otherwise = getDivisors (currentNum - 1) arr
+
+divides2 :: Integer -> [Integer]
+divides2 num
+    | num <= 0 = []
+    | otherwise = [n | n <- [1..num], isDivisor num n]
